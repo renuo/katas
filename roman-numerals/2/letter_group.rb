8f01a68f17168e7a7ef26f369b361699750281b5
Lukas Elmer <lukas.elmer@gmail.com>
2016-08-11 00:35:28 +0200

    extract letter group

---
+++ roman-numerals/2/letter_group.rb
@@ -0,0 +1,24 @@
+class LetterGroup
+  LETTER_GROUPS = [
+    [''] + %w(I II III IV V VI VII VIII IX),
+    [''] + %w(X XX XXX XL L LX LXX LXXX XC),
+    [''] + %w(C CC CCC CD D DC DCC DCCC CM),
+    [''] + %w(M MM MMM)]
+
+  def initialize(number, letter_group_number)
+    @number = number
+    @letter_group_number = letter_group_number
+  end
+
+  def letters
+    LETTER_GROUPS[letter_group_index][letters_index]
+  end
+
+  def letters_index
+    @number / @letter_group_number % 10
+  end
+
+  def letter_group_index
+    Math.log10(@letter_group_number).to_i
+  end
+end
--- roman-numerals/2/roman_numerals_converter.rb
+++ roman-numerals/2/roman_numerals_converter.rb
@@ -1,10 +1,4 @@
 class RomanNumeralsConverter
-  LETTER_GROUPS = [
-    [''] + %w(I II III IV V VI VII VIII IX),
-    [''] + %w(X XX XXX XL L LX LXX LXXX XC),
-    [''] + %w(C CC CCC CD D DC DCC DCCC CM),
-    [''] + %w(M MM MMM)]
-
   def initialize(number)
     @number = number
   end
@@ -16,16 +10,6 @@ class RomanNumeralsConverter
   private
 
   def letters_for(letter_group_number)
-    letter_group_index = letter_group_index(letter_group_number)
-    letters_index = letters_index(letter_group_number)
-    LETTER_GROUPS[letter_group_index][letters_index]
-  end
-
-  def letters_index(letter_group_number)
-    @number / letter_group_number % 10
-  end
-
-  def letter_group_index(letter_group_number)
-    Math.log10(letter_group_number).to_i
+    LetterGroup.new(@number, letter_group_number).letters
   end
 end
