747250bb55e9da7a96ca6952485117eaea698343
Lukas Elmer <lukas.elmer@gmail.com>
2016-08-15 16:50:37 +0100

    rubocop

--- roman-numerals/3/digit_to_roman_converter.rb
+++ roman-numerals/3/digit_to_roman_converter.rb
@@ -4,12 +4,12 @@ class DigitToRomanConverter
     [''] + %w(X XX XXX XL L LX LXX LXXX XC),
     [''] + %w(C CC CCC CD D DC DCC DCCC CM),
     [''] + %w(M MM MMM)
-  ]
+  ].freeze
 
   def initialize(number, digit_index)
-    @cut_number = number % (10 ** (digit_index + 1))
+    @cut_number = number % (10**(digit_index + 1))
     @roman_numbers = NUMBERS[digit_index]
-    @divisor = 10 ** digit_index
+    @divisor = 10**digit_index
   end
 
   def convert_digit_to_roman
