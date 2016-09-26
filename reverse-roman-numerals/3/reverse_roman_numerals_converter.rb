05084010521480a4e0c720b7967b820e276b7d6e
Lukas Elmer <lukas.elmer@renuo.ch>
2016-09-26 20:14:55 +0200

    oh yeah!! :dance:

---
+++ reverse-roman-numerals/3/reverse_roman_numerals_converter.rb
@@ -0,0 +1,15 @@
+module V3
+  class ReverseRomanNumeralsConverter
+    MAPPING = 'IV=4 IX=9 I=1 V=5 XL=40 XC=90 X=10 L=50 CD=400 CM=900 C=100 D=500 M=1000'
+                .split(' ').map { |x| x.split('=') }.to_h
+
+    def initialize(letters)
+      @letters = letters
+    end
+
+    def to_numeral
+      MAPPING.map { |roman, number| @letters.gsub!(roman, "#{number}|") }
+      @letters.split('|').map(&:to_i).inject(&:+)
+    end
+  end
+end
